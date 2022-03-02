b6e3c25780f20a738e8345d8327ba8308518b200
jonwk <johnwesleygithub@gmail.com>
2022-03-02 14:58:23 +0000

    Created Home and Login Pages

--- client/react-ts-app/src/App.tsx
+++ client/react-ts-app/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ROUTE, USER_PROFILE_ROUTE } from './routing';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE.HOME} element={<Home />} />
+        <Route path={ROUTE.LOGIN} element={<Login />} />
+        {/* <Route path={ROUTE.USERS} element={<Users />} />
+        <Route path={ROUTE.USER_PROFILE} element={<UserProfile />}>
+          <Route
+            path={USER_PROFILE_ROUTE.USER_ACTIVITY}
+            element={<UserActivity />}
+          />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/react-ts-app/src/components/Home.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { createPath, ROUTE } from '../routing';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            width: 400,
+            margin: `${theme.spacing(0)} auto`
+        },
+        loginBtn: {
+            marginTop: theme.spacing(2),
+            flexGrow: 1
+        },
+        header: {
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: 'center',
+            background: '#212121',
+            color: '#fff',
+        },
+        card: {
+            marginTop: theme.spacing(10)
+        }
+    })
+);
+
+const Home = () => {
+    const classes = useStyles();
+    const navigate = useNavigate();
+    return (
+        <Card className={classes.card}>
+            <CardHeader className={classes.header} title="Propylon Project Management Tool" />
+            <CardContent>
+                <h1>Home</h1>
+            </CardContent>
+            <CardActions>
+                <Button
+                    variant="contained"
+                    size="large"
+                    color="secondary"
+                    className={classes.loginBtn}
+                    onClick={() => { navigate(ROUTE.LOGIN); }}
+                >
+                    Login
+                </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default Home;
