5c752c23510a981231a2f0188ff05d38e63c6baa
Karthik Venkatesh <kvenkate@tcd.ie>
2022-03-16 13:03:06 +0000

    node js login branch created

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from 'body-parser';
-
+// import "./config/firebase-config; 
 import projectsRoutes from './routes/projects.js';
-
+import mongoose from "mongoose";
+import {} from 'dotenv/config';
 const app = express();
 const PORT = 8000;
 
@@ -12,7 +13,12 @@ app.use('/projects', projectsRoutes);
 
 app.get('/', (req, res) => {res.send('Hello from homepage!')});
 
+mongoose.connect(process.env.DB_CONNECTION,
+    {useNewUrlParser: true}, 
+    ()=> console.log('connected to db')
+);
+
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
-});
+}); 
