ca4d3c2cef269fbd4f15c19d81ea6166148f93e5
Tim Kelly <tkelly3118@gmail.com>
2022-03-16 20:08:54 +0000

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import mongoose from "mongoose";
+
 
 import projectsRoutes from './routes/projects.js';
 
 const app = express();
 const PORT = 8000;
+
+// connect to db
+mongoose.connect('mongodb+srv://admin:vGZL33etvNyiKSNb@cluster0.dszad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => 
+    console.log("Connect to DB")
+)
 
 app.use(bodyParser.json());
 
