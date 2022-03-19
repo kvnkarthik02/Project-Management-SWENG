606d369a602ec7582bd5fe99e2228a4181456ddd
Karthik Venkatesh <kvenkate@tcd.ie>
2022-03-19 21:05:25 +0000

    member schema commit

--- server/controllers/members.js
+++ server/controllers/members.js
@@ -37,7 +37,7 @@ export const deleteMemberById = (req,res) => {
 }
 
 // partial modification of project by id
-export const changeProjectById = (req,res) => {
+export const changeMemberById = (req,res) => {
     const { id } = req.params;
     const { FirstName } = req.body;
     const { LastName } = req.body;
@@ -48,3 +48,4 @@ export const changeProjectById = (req,res) => {
     if(LastName) member.lastName = LastName;
     // add more values
 }
+
--- server/routes/members.js
+++ server/routes/members.js
@@ -1,23 +1,24 @@
 import express from 'express';
-import { Project } from '../models/Project.js'
-import { getAllProjects, createProject, getProject, changeProject, deleteProject } from '../controllers/projects.js'
+import { Member } from '../models/Member.js'
+// import { getAllProjects, createProject, getProject, changeProject, deleteProject } from '../controllers/projects.js'
+import { changeMemberById, createMember, deleteMemberById, getAllMembers, getMemberById } from '../controllers/members.js';
 
 const router = express.Router();
 
 
 // get all projects
-router.get('/', getAllProjects);
+router.get('/', getAllMembers);
 
 //add new project
-router.post('/', createProject);
+router.post('/', createMember);
 
 // get project by id
-router.get('/:id', getProject);
+router.get('/:id', getMemberById);
 
 // delete by id
-router.delete('/:id', deleteProject);
+router.delete('/:id', deleteMemberById);
 
 // partial modification of project by id
-router.patch('/:id', changeProject);
+router.patch('/:id', changeMemberById);
 
 export default router;
