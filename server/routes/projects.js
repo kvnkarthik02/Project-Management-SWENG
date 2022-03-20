40ecf0e4cc360345e0df999db38e9360761d2c8e
Tim Kelly <tkelly3118@gmail.com>
2022-03-20 15:15:29 +0000

    fixed projects to work with db

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { Project } from '../models/Project.js'
-import { getAllProjects, createProject, getProject, changeProject, deleteProject } from '../controllers/projects.js'
+import { 
+    getAllProjects, 
+    createProject, 
+    getProject, 
+    changeProject, 
+    deleteProject 
+} from '../controllers/projects.js';
 
 const router = express.Router();
-
 
 // get all projects
 router.get('/', getAllProjects);
@@ -19,5 +23,15 @@ router.delete('/:id', deleteProject);
 
 // partial modification of project by id
 router.patch('/:id', changeProject);
+
+// get all tasks for a project
+//router.get('/', getAllTasks);
+
+// add a task to a project
+//router.post('/', createTasks);
+
+// edit a task
+//router.patch('/', changeTask);
+
 
 export default router;
