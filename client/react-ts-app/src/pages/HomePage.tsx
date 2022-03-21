0afa07859510d1a58a0b256b6bdd3c058132727c
jonwk <johnwesleygithub@gmail.com>
2022-03-21 17:04:12 +0000

    Finished working on TeamCard for Projects Page

--- client/react-ts-app/src/pages/HomePage.tsx
+++ client/react-ts-app/src/pages/HomePage.tsx
@@ -1,13 +1,48 @@
 import ListProjects from '../components/ListProjects';
 import ListEmployees from '../components/ListEmployees';
+import TeamCard from '../components/elements/TeamCard';
+
 import { Grid } from '@mantine/core';
+
+interface Employee {
+  name: string;
+  role: string;
+  workload: number;
+  capacity: number;
+  avatarColor: string;
+}
+
+const team: Employee[] = [
+  {
+    name: "Tim Kelly",
+    role: "Senior Developer",
+    workload: 6,
+    capacity: 16,
+    avatarColor: "red"
+  }, {
+    name: "Ben Vaughan",
+    role: "Senior Developer",
+    workload: 10,
+    capacity: 16,
+    avatarColor: "cyan"
+  }, {
+    name: "Johnny Shoe",
+    role: "Senior Developer",
+    workload: 13,
+    capacity: 16,
+    avatarColor: "yellow",
+  }
+]
 
 function HomePage() {
   return (
+    // <TeamCard {...team} />
     <Grid justify="center" columns={3} >
       <Grid.Col span={2}><ListProjects /></Grid.Col>
       <Grid.Col span={1}><ListEmployees /></Grid.Col>
     </Grid>
+
+
 
   )
 }
