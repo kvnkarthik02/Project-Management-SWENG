b9b5b091b8b6f85556a7c9a7f8f4613e9ce0ab26
Ben Vaughan <benovaughan@gmail.com>
2022-03-30 23:02:00 +0100

    Separate LayoutTest file to create reusable layout

---
+++ client/react-ts-app/src/components/layouts/elements/LayoutHeader.tsx
@@ -0,0 +1,18 @@
+const headerStyle = {
+    width: '100%',
+    paddingLeft: '150px',
+    paddingRight: '150px',
+    paddingTop: '15px',
+    paddingBottom: '15px',
+    backgroundColor: 'lightgrey'
+}
+
+function LayoutHeader() {
+    return(
+        <div style={headerStyle}>
+            Header
+        </div>
+    )
+}
+
+export default LayoutHeader;
