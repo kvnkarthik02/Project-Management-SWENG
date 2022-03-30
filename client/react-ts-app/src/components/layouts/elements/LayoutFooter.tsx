0b0a7e7ace104586309ce72892027f9a9456c6a5
Ben Vaughan <benovaughan@gmail.com>
2022-03-30 23:39:37 +0100

    removed footer

--- client/react-ts-app/src/components/layouts/elements/LayoutFooter.tsx
+++ client/react-ts-app/src/components/layouts/elements/LayoutFooter.tsx
@@ -1,12 +1,18 @@
-
+const footerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    paddingLeft: '150px',
+    paddingRight: '150px',
+    paddingTop: '20px',
+    paddingBottom: '20px',
+}
 
 function LayoutFooter() {
     return(
-        <div style={{ width: '100%', height:'30px', backgroundColor: 'blue'}}>
-            Footer
+        <div style={footerStyle}>
         </div>
     )
 }
-
 
 export default LayoutFooter;
