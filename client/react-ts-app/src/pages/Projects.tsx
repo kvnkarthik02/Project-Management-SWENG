aa3fa4f0be268750e8c27cff8b4e2d52b4636d7d
jonwk <johnwesleygithub@gmail.com>
2022-04-09 22:32:04 +0100

    Got working on the Teams endpoint

--- client/react-ts-app/src/pages/Projects.tsx
+++ client/react-ts-app/src/pages/Projects.tsx
@@ -3,18 +3,25 @@ import { AppService } from '../services/app.services';
 import { Card, Title, Stack, Box, Text, Group, List } from '@mantine/core';
 import ProjectAddModal from '../components/newComponents/ProjectAddModal';
 import ProjectCard from '../components/elements/ProejctCard';
+import ListProjects from '../components/ListProjects';
 
 
 function Projects() {
     let [projects, setProjects] = useState<any[]>([]);
+    let [members, setMembers] = useState<any[]>([]);
     const isAdmin = true;
 
     useEffect(() => {
         const getProjects = async () => {
             try {
-                const response = await AppService.getProjects();
-                setProjects(response);
-                console.log(response);
+                const projectsResponse = await AppService.getProjects();
+                // setProjects(projectsResponse);
+
+                const membersResponse = await AppService.getMembers();
+                setMembers(membersResponse);
+
+                console.log(membersResponse);
+
             } catch (err) {
                 console.log(err);
             }
@@ -23,7 +30,31 @@ function Projects() {
         getProjects();
     }, [])
 
+    // memberId: String,
+    // firstName: String,
+    // lastName: String,
+    // hoursAvailable: Number,
+    // hoursAllocated: Number,
+    // skills: [SkillSchema],
+    // projects: [String]
+
+    //     <List>
+    // {
+    //     projects? projects.map(project => (
+    //         <List.Item>
+    //             <div style={{
+    //                 margin: '5px',
+    //                 padding: '5px',
+    //             }}>
+    //                 {/* <ProjectCard project={project} isAdmin={isAdmin} /> */}
+    //             </div>
+    //         </List.Item>
+    //     ))
+    //                                 : < li > Loading...</li >}
+    //                         </List >
     return (
+        // <ListProjects />
+
         <div style={{ width: 710, margin: 'auto', padding: "5px" }}>
             <Card shadow="sm" p="lg" radius="md" withBorder={true}>
                 <div style={{ width: 510, margin: 'auto', padding: "5px" }}>
@@ -36,7 +67,7 @@ function Projects() {
                                 variant="text"
                                 size="xl"
                                 weight={700}
-                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}>Projects</Text>
+                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}>Team</Text>
                         </Title>
                         {/* {props.isAdmin && (<ProjectAddModal />)} */}
                         {isAdmin && (<ProjectAddModal />)}
@@ -46,15 +77,36 @@ function Projects() {
                         justifyContent: 'center'
                     }}>
                         <Box sx={{ maxWidth: 600 }} mx="auto">
-
                             <List>
-                                {projects ? projects.map(project => (
+                                {members ? members.map(member => (
                                     <List.Item>
                                         <div style={{
                                             margin: '5px',
                                             padding: '5px',
                                         }}>
-                                            <ProjectCard project={project} isAdmin={isAdmin} />
+                                            <Card shadow="sm" p="lg" withBorder={true}>
+
+                                                {/* // memberId: String,
+                                                    // firstName: String,
+                                                    // lastName: String,
+                                                    // hoursAvailable: Number,
+                                                    // hoursAllocated: Number,
+                                                    // skills: [SkillSchema],
+                                                    // projects: [String] */}
+
+                                                <h1>FirstName: <strong >{member.firstName}</strong></h1>
+                                                <h1>LastName: <strong >{member.lastName}</strong></h1>
+                                                <p>ID: <i>{member.memberId}</i></p>
+                                                <p>hoursAvailable: <b>{member.hoursAllocated}</b></p>
+                                                <p>hoursAllocated: <b>{member.hoursAllocated}</b></p>
+                                                <p>projects: <b>{JSON.stringify(member.projects, null, 2)}</b></p>
+                                                <p>skills: <b>{member.skills.map((skill: any) => (<div>{skill.name} - {skill.level}</div>))}</b></p>
+                                                {/* <p>skills: <b>{member.skills}</b></p> */}
+                                                {/* <p>isComplete: <b>{project.isComplete.toString()}</b></p>
+                                                <p>Description: <em>{parse(project.projectDescription)}</em></p>
+                                                <p>Tasks: {JSON.stringify(project.tasks, null, 2)}</p> */}
+                                            </Card>
+                                            {/* <ProjectCard project={project} isAdmin={isAdmin} /> */}
                                         </div>
                                     </List.Item>
                                 ))
@@ -64,7 +116,7 @@ function Projects() {
                         </Box>
 
                     </Stack>
-                </div>
+                </div >
 
             </Card >
         </div >
