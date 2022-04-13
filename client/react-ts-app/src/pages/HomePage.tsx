3f526fbe380a478873a0c885ab183c142120e0d0
Zemyna Kulikauskaite <kulikauz@tcd.ie>
2022-04-13 17:00:55 +0100

    pages

---
+++ client/react-ts-app/src/pages/HomePage.tsx
@@ -0,0 +1,42 @@
+import ListProjects from '../components/ListProjects';
+import ListEmployees from '../components/ListEmployees';
+import LayoutDefault from '../components/layouts/LayoutDefault';
+import ListTeam from '../components/ListTeam';
+import { Grid } from '@mantine/core';
+
+interface Employee {
+  name: string;
+  email: string;
+  role: string;
+  workload: number;
+  capacity: number;
+  avatarColor: string;
+}
+
+interface Project {
+  name: string;
+  description: string;
+  badgeName: string;
+  badgeColor: string;
+  // coverImgLink: string;
+  // projectPageLink: string;
+  //   workload: number;
+  //   capacity: number;
+  //   avatarColor: string;
+}
+
+
+const HomePage = (props: { team: Employee[]; projects: Project[]; }) => {
+  return (
+    <LayoutDefault>
+      <Grid justify="center" columns={3} >
+        <Grid.Col span={2}><ListProjects /></Grid.Col>
+        {/* <Grid.Col span={1}><ListEmployees team={props.team} /></Grid.Col> */}
+        <Grid.Col span={1}><ListTeam /></Grid.Col>
+      </Grid>
+
+    </LayoutDefault>
+  )
+}
+
+export default HomePage;
